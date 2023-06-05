27816441197e50108f59693f94424ca703484aba
lisaMariaX <88780000+lisaPolena@users.noreply.github.com>
2023-06-05 13:26:00 +0200

    feat: home - get all pins

--- src/components/general/Navbar.tsx
+++ src/components/general/Navbar.tsx
@@ -11,7 +11,7 @@ export const Navbar = () => {
   return (
     <>
       <div className={`fixed inset-x-0 bottom-0 flex justify-between bg-black ${loadDeleteBoardTransaction ? 'z-20' : ''}`}>
-        <Link href={''} className='flex justify-center w-full px-3 py-5 ml-10 align-center'>
+        <Link href={'/home'} className='flex justify-center w-full px-3 py-5 ml-10 align-center'>
           <div className='text-2xl'><IoHome /></div>
         </Link>
         <Link href={''} className='flex justify-center w-full px-3 py-5 align-center'>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,24 +1,36 @@
 import Head from 'next/head'
 import { Navbar } from '@/components/general/Navbar'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useWeb3React } from '@web3-react/core';
 import { useRouter } from 'next/router';
+import { usePinManager } from '@/common/functions/contracts';
 
 export default function Home() {
   // active: returns a boolean to check if user is connected
-  const { active } = useWeb3React()
+  const { active, library } = useWeb3React()
   const router = useRouter();
+  const pinManagerContract = usePinManager(library);
+  const [pins, setPins] = useState<any[]>([]);
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
       if (!active) router.push('/');
     }, 2000);
 
+    getAllPins();
+
     return () => {
       clearTimeout(timeoutId);
     };
 
-  }, [active])
+  }, [active, library])
+
+  function getAllPins() {
+    pinManagerContract?.getAllPins().then((result: any) => {
+      const pins = result.map((pin: any) => ({ id: pin.id.toNumber(), title: pin.title, description: pin.description, imageHash: pin.imageHash, boardId: pin.boardId.toNumber(), owner: pin.owner }));
+      setPins(pins);
+    });
+  }
 
   return (
     <>
@@ -28,10 +40,22 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className='h-screen bg-black'>
-        Some Content
-        bla
-        bla
+      <main className='h-screen overflow-auto bg-black'>
+        <div className={`fixed inset-x-0 top-0 grid grid-cols-3 bg-black h-[50px] pt-3 px-2 z-10`}>
+        </div>
+
+        <div className="grid grid-cols-2 gap-3 px-4 relative top-[50px] mt-2">
+          {pins.map((pin: any) => (
+            <div key={pin.id} className="h-auto" onClick={() => router.push(`/pin/${pin.id}`)}>
+              <img src={`https://web3-pinterest.infura-ipfs.io/ipfs/${pin.imageHash}`}
+                alt={pin.title} className="object-cover w-full rounded-2xl max-h-72" />
+              <div className='mb-4'>
+                <h2 className="pt-2 pl-2 text-white font-semibold text-[0.9rem]">{pin.title}</h2>
+              </div>
+            </div>
+          ))}
+        </div>
+
       </main>
       <Navbar />
     </>
