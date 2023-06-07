a17589365840aa7aae8850dcb6fed49d4703ed45
lisaMariaX <88780000+lisaPolena@users.noreply.github.com>
2023-06-08 01:51:55 +0200

    feat: edit pin modal

--- src/components/overlays/EditPinModal.tsx
+++ src/components/overlays/EditPinModal.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Modal from '../general/Modal';
 import { useAppState } from '../general/AppStateContext';
-import { Input, Select } from '@chakra-ui/react';
+import { Input, List, ListItem, Select, Slide } from '@chakra-ui/react';
 import DeleteModal from './DeleteModal';
 import { useRouter } from 'next/router';
 import boardManager from '../../contracts/build/BoardManager.json';
 import pinManager from '../../contracts/build/PinManager.json';
 import { useAccount, useContractEvent, useContractRead, useContractWrite, usePrepareContractWrite } from 'wagmi';
 import { Board } from '@/common/types/structs';
+import { IoChevronBack, IoCheckmarkSharp, IoChevronForward } from "react-icons/io5";
 
 interface EditPinModalProps {
     pin: any;
@@ -17,13 +18,15 @@ const EditBoardModal: React.FC<EditPinModalProps> = (props: EditPinModalProps) =
     const { pin } = props;
     const { address, isConnected } = useAccount()
     const { editPinModalOpen, setEditPinModalOpen, deletePinModalOpen, setDeletePinModalOpen } = useAppState();
-    const [pinTitle, setPinTitle] = React.useState<string>('');
-    const [pinDescription, setPinDescripton] = React.useState<string>('');
-    const [pinBoardId, setPinBoardId] = React.useState<string | number>('');
-    const [newPinBoardId, setNewPinBoardId] = React.useState<string>('');
-    const [isOwner, setIsOwner] = React.useState<boolean>(false);
-    const [boards, setBoards] = React.useState<Board[]>([]);
+    const [pinTitle, setPinTitle] = useState<string>('');
+    const [pinDescription, setPinDescripton] = useState<string>('');
+    const [pinBoardId, setPinBoardId] = useState<string | number>('');
+    const [newPinBoardId, setNewPinBoardId] = useState<string | number>('');
+    const [isOwner, setIsOwner] = useState<boolean>(false);
+    const [boards, setBoards] = useState<Board[]>([]);
     const router = useRouter();
+    const [boardSlideOpen, setBoardSlideOpen] = useState<boolean>(false);
+    const [board, setBoard] = useState<Board | null>(null);
 
     const { data: allBoardsByAddress } = useContractRead({
         address: `0x${process.env.NEXT_PUBLIC_BOARD_MANAGER_CONTRACT}`,
@@ -31,7 +34,12 @@ const EditBoardModal: React.FC<EditPinModalProps> = (props: EditPinModalProps) =
         functionName: 'getBoardsByOwner',
         args: [address],
         onSuccess(data) {
-            setBoards(data as Board[]);
+            const res = data as Board[];
+            setBoards(res);
+            if (pinBoardId) {
+                const b = res.find((board: Board) => Number(board.id) === pinBoardId) ?? null;
+                setBoard(b);
+            }
         },
     });
 
@@ -74,7 +82,7 @@ const EditBoardModal: React.FC<EditPinModalProps> = (props: EditPinModalProps) =
             setPinBoardId(Number(boardId));
         }
 
-    }, [pin, address, allBoardsByAddress, editSavedPinStatus, editCreatedPinStatus]);
+    }, [pin, address, allBoardsByAddress, editSavedPinStatus, editCreatedPinStatus, newPinBoardId]);
 
     //TODO created edit pin event 
     async function editCreatedPin() {
@@ -94,6 +102,12 @@ const EditBoardModal: React.FC<EditPinModalProps> = (props: EditPinModalProps) =
         router.push('/profile');
     }
 
+    const handleNewBoard = (board: Board) => {
+        setNewPinBoardId(Number(board.id));
+        setBoardSlideOpen(false);
+        setBoard(board);
+    }
+
     return (
         <>
             {deletePinModalOpen && <div className='absolute top-0 w-full h-full z-[12] bg-zinc-800 opacity-70'></div>}
@@ -109,37 +123,89 @@ const EditBoardModal: React.FC<EditPinModalProps> = (props: EditPinModalProps) =
                 </div >
                 <div className='flex flex-col gap-4'>
                     <div>
-                        <img src={`https://web3-pinterest.infura-ipfs.io/ipfs/${pin?.imageHash}`} alt={pin?.title} className='object-cover w-40 h-40 rounded-2xl' />
+                        <img src={`https://web3-pinterest.infura-ipfs.io/ipfs/${pin?.imageHash}`} alt={pin?.title}
+                            className="object-cover m-auto w-80 h-80 rounded-2xl" />
                     </div>
                     {isOwner &&
-                        <>
-                            <div>
-                                <p>Title</p>
-                                <Input variant='unstyled' placeholder='Add' defaultValue={pinTitle} onChange={(e) => setPinTitle(e.target.value)} />
+                        <div className='flex flex-col mx-4'>
+                            <div className='mt-4'>
+                                <p className='text-lg font-semibold'>Title</p>
+                                <Input variant='unstyled' placeholder='Add' defaultValue={pinTitle} size={'lg'}
+                                    onChange={(e) => setPinTitle(e.target.value)} />
                             </div>
 
-                            <div>
-                                <p>Description</p>
-                                <Input variant='unstyled' placeholder='Add what your board is about' defaultValue={pinDescription} onChange={(e) => setPinDescripton(e.target.value)} />
+                            <div className='mt-4'>
+                                <p className='text-lg font-semibold'>Description</p>
+                                <Input variant='unstyled' placeholder='Add what your board is about' size={'lg'}
+                                    defaultValue={pinDescription} onChange={(e) => setPinDescripton(e.target.value)} />
                             </div>
-                        </>
+                        </div>
                     }
 
-                    <div>
-                        <Select placeholder='Select option' value={newPinBoardId !== '' ? newPinBoardId : pinBoardId} onChange={(e) => setNewPinBoardId(e.target.value)}>
-                            {boards.map((board) =>
-                                <option key={board.id} value={Number(board.id)}>{board.name}</option>
-                            )}
-                        </Select>
+                    <div className='flex flex-col mx-4 mb-[-0.5rem]'>
+                        <p className='text-sm font-semibold'>Board</p>
+                    </div>
+                    <div className='flex items-center mx-4 border-white' onClick={() => setBoardSlideOpen(true)}>
+                        {board ? (
+                            <>
+                                {pin && pin.imageHash ? (
+                                    <img className='w-12 h-12 rounded-xl'
+                                        src={`https://web3-pinterest.infura-ipfs.io/ipfs/${pin.imageHash}`} alt='board' />
+                                ) : (
+                                    <div className='w-12 h-12 bg-gray-200 rounded-xl'></div>
+                                )}
+                                <h2 className='ml-4 text-lg font-bold'>{board.name}</h2>
+                                <IoChevronForward size={30} className='absolute right-5' />
+                            </>
+                        ) : (
+                            <>
+                                <h2 className='ml-4 text-lg font-bold'>Choose Board</h2>
+                                <IoChevronForward size={30} className='absolute right-5' />
+                            </>
+                        )}
+
                     </div>
 
-                    <div className='flex justify-between item-center' onClick={() => setDeletePinModalOpen(true)}>
-                        <div>
-                            <p className='font-bold'>Delete this Pin</p>
-                        </div>
+                    <div className='flex mt-6 ml-4' onClick={() => setDeletePinModalOpen(true)}>
+                        <p className='text-lg font-bold'>Delete this Pin</p>
                     </div>
 
                 </div>
+
+                <Slide direction='right' in={boardSlideOpen} style={{ zIndex: 20 }}>
+                    <div className="fixed bottom-0 left-0 right-0 p-4 bg-zinc-800 rounded-t-[40px] z-10 h-full">
+                        <div className="flex items-center justify-between gap-24 flex-cols">
+                            <button className="text-white" onClick={() => setBoardSlideOpen(false)}>
+                                <IoChevronBack size={30} />
+                            </button>
+                        </div>
+                        <div className="flex items-center gap-24 mt-4">
+                            <h2 className="text-base text-white">Your Boards</h2>
+                        </div>
+                        <List>
+                            {boards.map((board: any) => (
+                                <ListItem key={Number(board.id)} onClick={() => handleNewBoard(board)}
+                                >
+                                    <div className='flex items-center h-16'>
+
+                                        {board.pins?.length > 0 && board.pins[0].imageHash ? (
+                                            <img className='w-14 h-14 rounded-xl' src={`https://web3-pinterest.infura-ipfs.io/ipfs/${board.pins[0].imageHash}`} alt='board' />
+                                        ) : (
+                                            <div className='bg-gray-200 w-14 h-14 rounded-xl'></div>
+                                        )}
+
+
+                                        <div className='justify-center ml-4'>
+                                            <h2 className='text-lg font-bold'>{board.name}</h2>
+                                        </div>
+
+                                        {newPinBoardId && newPinBoardId === Number(board.id) || !newPinBoardId && Number(board.id) === pinBoardId ? <IoCheckmarkSharp size={30} className='absolute right-6' /> : ''}
+                                    </div>
+                                </ListItem>
+                            ))}
+                        </List>
+                    </div>
+                </Slide>
             </Modal>
             <DeleteModal isOpen={deletePinModalOpen} closeModal={() => setDeletePinModalOpen(false)} isBoard={false} pin={pin} isOwner={isOwner} savedPinBoardId={pinBoardId} />
         </>
